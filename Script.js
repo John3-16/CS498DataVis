ec0a0aa67c596b0ba6e9cbd7fa5eafa086d66e90
Amazing Grace <18647491+John3-16@users.noreply.github.com>
2019-08-03 12:39:53 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,40 +1,67 @@
+var displayResources = $("#display-resources");
+var massdata;
+
 function unique(data) {
-    temp = {}
-    for (var i=0; i < data.length; i++) {
-        year = new Date(data[i].Date).getFullYear();
-        temp[year] = 1;
+    let temp = [] // object definition
+    for (let i=0; i < data.length; i++) {
+        let row = data[i];
+        let year = new Date(row.Date).getFullYear();
+        let idx = temp.findIndex(o=> o.Year === year);
+        if (idx != -1) {
+            temp[idx].attacks++;
+        } else {
+            let Year = {Year: year, attacks: 1};
+            let Attack = {Date: row.Date, Country: row.Country, State: row.State, City: row.City,  Killed: row.Killed,
+                          Perpetrator: row.Perpetrator, Type: row.Type, Infomation: row.InfomationOnAttack};
+            temp.push(Year);
+        }
     }
-    return Object.keys(temp);
+    return temp;
 }//unique()
 
-$(document).ready(function() {
-    $("#retrieve-resources").click(function() {
-        var displayResources = $("#display-resources");
-        displayResources.text("Loading data from JSON sources...");
-    
-        function success(resp1, resp2) {
-            var usms = resp1[0];
-            var worldms = resp2[0];
-            displayResources.text("Data loaded successfully...");
+function getMaxValue(darray, ele) {
+    let values = [];
+    for(let i=0; i < darray.length; i++) {
+        values.push(Math.ceil(parseFloat(darray[i][""+ele])));
+    }
+    values.sort(function (a,b) {return a-b})
+    return values[values.length-1]
+}//getMaxValue()
 
-            var logScale = d3.scaleLog()
-                .domain([10, 150])
-                .range([0,200]);
-            var svg = d3.select("svg");
-            var raw = worldms.Appendix_restOfTheWorld;
-            var years = unique(raw);
-            console.log(years);
+function getJSON(url) {
+    return get(url).then(JSON.parse(response));
+}//getJSON()
+
+function getData() {
+    let usmsajax = $.ajax({type: "GET", url: "https://john3-16.github.io/CS498DataVis/USMassShooting.json"});
+    let worldmsajax = $.ajax({type: "GET", url: "https://john3-16.github.io/CS498DataVis/WorldMassShooting.json"});
+    
+    function success(resp1, resp2) {
+        let usms = resp1[0];
+        let worldms = resp2[0];
+        displayResources.text("Data loaded successfully...");
         
-        };//success()
+        massdata = unique(worldms.Appendix_restOfTheWorld);
+    };//success()
     
-        function failure(response1, response2) {
-            displayResources.text("Something is wrong with loading the data ");
-        };//failure()
+    function failure(response1, response2) {
+        displayResources.text("Something is wrong with loading the data ");
+    };//failure()
+    
+    $.when( usmsajax, worldmsajax )
+        .then( success, failure );
+}//getData()
 
-        let usmsajax = $.ajax({type: "GET", url: "https://john3-16.github.io/CS498DataVis/USMassShooting.json"});
-        let worldmsajax = $.ajax({type: "GET", url: "https://john3-16.github.io/CS498DataVis/WorldMassShooting.json"});
+$(document).ready(function() {
+    displayResources.text("Loading data from JSON sources...");
+    var data = getData();
+    console.log(data);
     
-        $.when( usmsajax, worldmsajax )
-            .then( success, failure );
+    var logScale = d3.scaleLog()
+        .domain([10, 150])
+        .range([0,200]);
+    var svg = d3.select("svg");
+    
+    $("#retrieve-resources").click(function() {
     });//click function
 });//document ready function
